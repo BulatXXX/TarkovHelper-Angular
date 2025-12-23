2d3660481cefba4833dad67479c7c1b6ae116f6f
Sattarov Bulat <90152783+BulatXXX@users.noreply.github.com>
2025-12-23 04:46:40 +0300

    decorations

--- src/app/features/items/pages/items-search/ui/items-search.ts
+++ src/app/features/items/pages/items-search/ui/items-search.ts
@@ -1,5 +1,5 @@
-import {Component, DestroyRef, ElementRef, inject} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Component, DestroyRef, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {ItemSearchViewModel} from '../item-search-view-model';
@@ -7,14 +7,14 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule, NgOptimizedImage],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './items-search.html',
   styleUrl: './items-search.scss',
 })
 export class ItemsSearch {
   private vm = inject(ItemSearchViewModel);
   private destroyRef = inject(DestroyRef);
-  private host = inject(ElementRef<HTMLElement>);
+
 
   query = new FormControl(this.vm.getState.query, { nonNullable: true });
   viewModel$ = this.vm.state$;
@@ -29,21 +29,13 @@ export class ItemsSearch {
   // если сделаешь переключатель режима:
   setMode(mode: 'pve' | 'regular') { this.vm.setMode(mode); }
 
-  onFocusIn() {
-    this.vm.setShowHistory(true);
-  }
-
-  onFocusOut(event: FocusEvent) {
-    const next = event.relatedTarget as HTMLElement | null;
-
-    // если фокус ушёл вообще "в никуда" или на элемент вне контейнера — закрываем
-    if (!next || !this.host.nativeElement.contains(next)) {
-      this.vm.setShowHistory(false);
-    }
-  }
 
   clearHistory() {
     this.vm.clearHistory();
+  }
+
+  get isQueryEmpty(): boolean {
+    return this.query.value.trim().length === 0;
   }
 
   placeholder = 'data:image/gif;base64,R0lGODlhAQABAAAAACw=';
